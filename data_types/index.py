3bde9915b0cc25f4338d09858682cb358e4306de
Ironpurush0 <ridamsaxena90.com>
2020-10-22 22:40:17 +0530

    .

---
+++ data_types/index.py
@@ -0,0 +1,9 @@
+# There are three different variables that holds different values of different data types.
+brand = "Starbucks"
+outlets = 20
+items = [10, 20, 30, 40]
+
+# type() method is used to find out the data type of a variable or data type of the value that has been assigned to that variable.
+print(type(brand))
+print(type(outlets))
+print(type(items))
