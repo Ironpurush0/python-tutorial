244d1af7fb8de062e3a3b2473d909eb474d92435
Ironpurush0 <ridamsaxena90.com>
2020-10-22 21:04:41 +0530

    data types.

--- variables/index.py
+++ variables/index.py
@@ -1,2 +1,34 @@
+# This is an example of simple variable. Variables are just placeholders which allows you to asign any value to it. 
 a = 2
 print(a)
+
+# This variable holds a value which is of string type.
+variable = "This is a variable."
+print(variable)
+
+# These are two different variables that holds two different values where one is a string type and other is int type.
+name, age = "Jem", 17
+print(name, age)
+
+# This variable holds an array which is known as list in python.
+data = [1,2,4,5]
+print(data)
+
+# NAMING VARIABLES 
+# You can define a single word as a variable name but if your variable name consists more then one word than you can do something like this.
+
+# This approach is called camelCase naming convension where the first letter of the second word should be capital
+firstName = "James"
+print(firstName)
+
+# Or you can use an underscore. 
+# Try not to use any special characters like & or $ as it will not work.
+first_name = "John"
+print(first_name)
+
+
+# You can also define the name is all uppercase as these variables are mostly holds a constant value which never changes.
+PI = 3.14
+print(PI)
+
+
